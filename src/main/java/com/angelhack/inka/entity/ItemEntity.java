f1437992694a888f371e69cc63ee4a6ebf91ea99
grtsantu <tanmoy.1984@yahoo.com>
2015-06-21 05:18:08 +0800

    commit

--- src/main/java/com/angelhack/inka/entity/ItemEntity.java
+++ src/main/java/com/angelhack/inka/entity/ItemEntity.java
@@ -1,101 +1,20 @@
 package com.angelhack.inka.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.Collection;
+import java.util.List;
 
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import com.angelhack.inka.common.ItemCategory;
-
-import javax.persistence.*;
-
-import java.util.Collection;
-import java.util.List;
-
-/**
- * Created by Mikhail_Voloshin on 6/20/2015.
- */
-@Entity
-public class ItemEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String description;
-
-    @ElementCollection(targetClass = ItemCategory.class)
-    @CollectionTable(name = "ITEM_CATEGORY",
-            joinColumns = @JoinColumn(name = "ITEM_ID"))
-    @Column(name = "CATEGORY_ID")
-    private List<ItemCategory> category;
-
-    @JsonIgnore
-    @ManyToOne
-    private WishlistEntity wishlist;
-    
-    @OneToMany(mappedBy="item")
-    private Collection<DiscountEntity> discounts;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public WishlistEntity getWishlist() {
-        return wishlist;
-    }
-
-    public void setWishlist(WishlistEntity wishlist) {
-        this.wishlist = wishlist;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public List<ItemCategory> getCategory() {
-        return category;
-    }
-
-    public void setCategory(List<ItemCategory> category) {
-        this.category = category;
-    }
-
-	public Collection<DiscountEntity> getDiscounts() {
-		return discounts;
-	}
-
-	public void setDiscounts(Collection<DiscountEntity> discounts) {
-		this.discounts = discounts;
-	}
-    
-    
-}
-package com.angelhack.inka.entity;
-
-import com.angelhack.inka.common.ItemCategory;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import javax.persistence.*;
-import java.util.List;
 
 /**
  * Created by Mikhail_Voloshin on 6/20/2015.
@@ -119,6 +38,9 @@ public class ItemEntity {
     @ManyToOne
     @JoinColumn(name = "WISHLIST_ID")
     private WishlistEntity wishlist;
+    
+    @OneToMany(mappedBy="item")
+    private Collection<DiscountEntity> discounts;
 
     public Long getId() {
         return id;
@@ -175,4 +97,14 @@ public class ItemEntity {
     public void setBroadcast(Boolean broadcast) {
         this.broadcast = broadcast;
     }
+
+	public Collection<DiscountEntity> getDiscounts() {
+		return discounts;
+	}
+
+	public void setDiscounts(Collection<DiscountEntity> discounts) {
+		this.discounts = discounts;
+	}
+    
+    
 }
