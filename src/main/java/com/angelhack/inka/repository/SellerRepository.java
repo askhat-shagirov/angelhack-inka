25eeb9df92a2e0e2c5626147be3f5a932319731b
Askhat Shagirov <askhat_shagirov@epam.com>
2015-06-20 21:59:30 +0600

    Added repository annotation

--- src/main/java/com/angelhack/inka/repository/SellerRepository.java
+++ src/main/java/com/angelhack/inka/repository/SellerRepository.java
@@ -2,10 +2,12 @@ package com.angelhack.inka.repository;
 
 import com.angelhack.inka.entity.SellerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Askhat_Shagirov on 20-Jun-15.
  */
+@Repository
 public interface SellerRepository extends JpaRepository<SellerEntity, Long> {
 
     SellerEntity findByEmail(String email);
